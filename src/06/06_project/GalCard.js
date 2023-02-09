a3f1f0935c4208432277054bbb4f27c1ffd1f6dd
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-02-09 20:32:57 +0900

    0209_ver3

--- src/06/06_project/GalCard.js
+++ src/06/06_project/GalCard.js
@@ -3,11 +3,10 @@ const GalCard = ({cardItem}) => {
     return (
         <div className="concard"> 
             <div><img src={cardItem.galWebImageUrl} /></div>
-            <div>{cardItem.galTitle}</div>  
+            <div className="contitle">{cardItem.galTitle}</div>  
             <div className="connm">{cardItem.galPhotographyMonth.substr(0,4)}.{cardItem.galPhotographyMonth.substr(4,2)}.</div> 
             <div className="connm">{cardItem.galPhotographer}</div>  
             <div className="conkw">{cardItem.galSearchKeyword}</div>
-
         </div>
     ) ;
 }
