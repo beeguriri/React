cd9b1b9ac29299174e7d85fb7fbd94dde69968b0
Hyejin Kang <122770696+beeguriri@users.noreply.github.com>
2023-02-11 11:17:27 +0900

    w1 정리 끝

--- src/10/W1.js
+++ src/10/W1.js
@@ -4,11 +4,11 @@ import weather1 from '../db/weather2.json'
 
 const W1 = () => {
 
-    console.log('weather1', weather1)
-    console.log('keys', keys)
+//    console.log('weather1', weather1)
+//    console.log('keys', keys)
 
     let items = weather1.response.body.items.item;
-    console.log('items', items)
+//    console.log('items', items) : item 8개
 
     //화면 출력용 배열 만들고자 함
     //map의 return함수 사용
@@ -19,22 +19,36 @@ const W1 = () => {
         temp.push(i.obsrValue);         //itmes의 obsrBalue값(obj)
         temp.push(keys[i.category][1]); //items의 category에 해당하는 key값(배열) => index 1번째 값
         return temp;
-
     });
 
-    console.log('w1', w1)
+//    console.log('w1', w1);
+//    console.log('w1[0]', w1[0]);
+
+    let w1list = w1.map((item) => {
+
+        if(item[2] === "코드값")
+            return <div className="w1div" key={item[0]}>
+                        <span className="sp0">{item[0]}</span>
+                        <span className="sp1">없음</span>
+                        <span className="sp2"></span>
+                    </div>
+
+        return <div className="w1div" key={item[0]}>
+                        <span className="sp0">{item[0]}</span>
+                        <span className="sp1">{item[1]}</span>
+                        <span className="sp2">{item[2]}</span>
+
+                </div>
+    });
+
 
     return(
         <>
         <div className="content">
             <Wheader title={'일기예보-단기'} />
             
-            <div>
-                <div className="w1div"> 
-                    <span className="sp0">강수량</span>
-                    <span className="sp1">0</span>
-                    <span className="sp2">mm</span>
-                </div>
+            <div className="w1">
+                {w1list}
             </div>
         </div>
         </>
